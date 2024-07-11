bb5eff394a360def639b59ab0e68e43498f196d2
Alexeeva <alekseevamo@basealt.ru>
2024-07-11 19:34:59 +0400

    Updated .gitignore

--- gvdb/gvdb/g-ir-cpp-z4o_fjnl.c
+++
@@ -1,6 +0,0 @@
-#ifndef __GI_SCANNER__
-# define __GI_SCANNER__
-#endif
-#include </home/DOMAIN.TEST/administrator/Develop/gvdb_pygobject/gvdb/gvdb/gvdb-builder.h>
-#include </home/DOMAIN.TEST/administrator/Develop/gvdb_pygobject/gvdb/gvdb/gvdb-format.h>
-#include </home/DOMAIN.TEST/administrator/Develop/gvdb_pygobject/gvdb/gvdb/gvdb-reader.h>
